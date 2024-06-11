567c42320974b64bbd237581544a36a45c5ee448
Patrycja Wierzbicka <patrycja.wierzbicka07@gmail.com>
2024-06-11 13:16:36 +0200

    ajouts des routers pour chaque page

---
+++ kasa/src/composants/accueil/accueil.jsx
@@ -0,0 +1,14 @@
+import React from 'react' 
+
+export default function Header() {
+    return <>
+  
+   <Page></Page>
+  
+    </>
+  }
+  
+  function Page () {
+    return <h1>Premiere page !</h1>
+  }
+  
---
+++ kasa/src/composants/ficheslogements/ficheslogements.jsx
@@ -0,0 +1,14 @@
+import React from 'react' 
+
+export default function Header() {
+    return <>
+  
+   <FicheLog></FicheLog>
+  
+    </>
+  }
+  
+  function FicheLog () {
+    return <h1>Fiches logements</h1>
+  }
+  
